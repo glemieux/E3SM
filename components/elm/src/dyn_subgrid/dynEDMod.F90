362159a1bf24cd8450cb017c83a274730d7a11cc
Gregory Lemieux <glemieux@users.noreply.github.com>
2025-02-11 16:31:16 -0800

    update dyn_ed for fates multicolumn mode
    This removes the active column check as the reweight subroutines that
    follow the dyn_ed call need to have insight on how the weights have
    changed prior to setting subgrid data structures as active (or not)

--- components/elm/src/dyn_subgrid/dynEDMod.F90
+++ components/elm/src/dyn_subgrid/dynEDMod.F90
@@ -7,6 +7,9 @@ module dynEDMod
   use landunit_varcon, only : istsoil
   use VegetationType      , only : veg_pp
   use ColumnType     , only : col_pp
+  use elm_varctl, only : use_fates_multicolumn
+  use elm_varctl, only : iulog
+
   !
   ! !PUBLIC MEMBER FUNCTIONS:
   implicit none
@@ -30,11 +33,20 @@ contains
     
     do p = bounds%begp,bounds%endp
        c = veg_pp%column(p)
-       if (col_pp%itype(c) == istsoil .and. col_pp%active(c) ) then 
+       if (col_pp%itype(c) == istsoil) then
           if ( veg_pp%is_veg(p) .or. veg_pp%is_bareground(p)) then
-             veg_pp%wtcol(p) = veg_pp%wt_fates(p)
+             ! If using singlesite multicolumn option, fates has only one patch
+             ! per column and we leave the wtcol as one for all patches and
+             ! directly manipulate the column weight relative to the landunit
+             if (trim(use_fates_multicolumn) == "singlesite") then
+                col_pp%wtlunit(c) = veg_pp%wt_fates(p)
+             else
+                veg_pp%wtcol(p) = veg_pp%wt_fates(p)
+             end if
           else
-             veg_pp%wtcol(p) = 0.0_r8 
+             if (trim(use_fates_multicolumn) .ne. "singlesite") then
+                veg_pp%wtcol(p) = 0.0_r8 
+             end if
           end if
        end if
     end do
