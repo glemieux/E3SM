1137814d098d1c839a356fdc5e225d65d27ff493
Gregory Lemieux <glemieux@users.noreply.github.com>
2026-05-22 09:51:01 -0700

    add clapp and hornberger to fates interface registry

--- components/elm/src/main/elmfates_interfaceMod.F90
+++ components/elm/src/main/elmfates_interfaceMod.F90
@@ -1339,7 +1339,6 @@ contains
             this%fates(nc)%bc_in(s)%watsat_sisl(1:nlevsoil) = soilstate_inst%watsat_col(c,1:nlevsoil)
             this%fates(nc)%bc_in(s)%watres_sisl(1:nlevsoil) = soilstate_inst%watmin_col(c,1:nlevsoil)
             this%fates(nc)%bc_in(s)%sucsat_sisl(1:nlevsoil) = soilstate_inst%sucsat_col(c,1:nlevsoil)
-            this%fates(nc)%bc_in(s)%bsw_sisl(1:nlevsoil)    = soilstate_inst%bsw_col(c,1:nlevsoil)
          end if
 
          ! get the harvest data, which is by gridcell
@@ -2025,9 +2024,6 @@ contains
                      this%fates(nc)%bc_in(s)%sucsat_sisl(1:nlevsoil) = &
                           soilstate_inst%sucsat_col(c,1:nlevsoil)
 
-                     this%fates(nc)%bc_in(s)%bsw_sisl(1:nlevsoil) = &
-                          soilstate_inst%bsw_col(c,1:nlevsoil)
-
                   end do
 
                   call RestartHydrStates(this%fates(nc)%sites,  &
@@ -2177,9 +2173,6 @@ contains
 
                  this%fates(nc)%bc_in(s)%sucsat_sisl(1:nlevsoil) = &
                       soilstate_inst%sucsat_col(c,1:nlevsoil)
-
-                 this%fates(nc)%bc_in(s)%bsw_sisl(1:nlevsoil) = &
-                      soilstate_inst%bsw_col(c,1:nlevsoil)
 
                  do j = 1, nlevsoil
                     vol_ice = min(soilstate_inst%watsat_col(c,j), &
@@ -3621,8 +3614,6 @@ end subroutine wrap_update_hifrq_hist
            soilstate_inst%watmin_col(c,1:nlevsoil)
       this%fates(nc)%bc_in(s)%sucsat_sisl(1:nlevsoil)     = &
             soilstate_inst%sucsat_col(c,1:nlevsoil)
-      this%fates(nc)%bc_in(s)%bsw_sisl(1:nlevsoil)        = &
-            soilstate_inst%bsw_col(c,1:nlevsoil)
 
       do ifp = 1, this%fates(nc)%sites(s)%youngest_patch%patchno
          p = ifp+col_pp%pfti(c)
@@ -4157,6 +4148,10 @@ subroutine RegisterInterfaceVariablesColdStart(this, nc, canopystate_inst, soils
                                                   subgrid_type=registry_var_intid_column)
          call this%fates(nc)%registry(r)%Register(key=hlm_fates_soil_saturated_hydr_cond, &
                                                   data=soilstate_inst%hksat_col(c,:), &
+                                                  hlm_flag=.true., &
+                                                  subgrid_type=registry_var_intid_column)
+         call this%fates(nc)%registry(r)%Register(key=hlm_fates_soil_clapp_hornberger_b, &
+                                                  data=soilstate_inst%bsw_col(c,:), &
                                                   hlm_flag=.true., &
                                                   subgrid_type=registry_var_intid_column)
       end if
